e84856575ff05f0e76953c0ac01a87fd6519f4f1
Yanick Schoch <51885072+Lemi0002@users.noreply.github.com>
2023-07-03 14:25:55 +0200

    Add functions to global file

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -1,3 +1,62 @@
+local globals = {}
+
 P = function(argument)
     print(vim.inspect(argument))
 end
+
+globals.get_file_names = function()
+    local keyword = vim.fn.input('Keyword: ')
+    local position = vim.api.nvim_win_get_cursor(0)
+
+    vim.fn.jobstart('rg --files --sort path | rg --fixed-strings "' .. keyword .. '"', {
+        stdout_buffered = true,
+        on_stdout = function(_, data)
+            vim.api.nvim_buf_set_lines(0, position[1], position[1], false, data)
+        end,
+    })
+end
+
+globals.export_current_file_name = function()
+    local name = vim.fn.expand('%:t')
+    vim.fn.setreg('+', name)
+    print('Copied file name to system clipboard')
+end
+
+globals.export_current_file_path = function()
+    local path = vim.fn.expand('%')
+    vim.fn.setreg('+', path)
+    print('Copied file path to system clipboard')
+end
+
+globals.run_current_file = function()
+    local extension = 'build.bat'
+    local delimiter = '_'
+    local path = vim.fn.expand('%:h')
+    local name_full = vim.fn.expand('%:t')
+    local name_cropped = vim.fn.expand('%:t:r')
+    local file = name_cropped .. delimiter .. extension
+    local file_run = vim.fn.expand(path .. '/' .. file)
+    local file_run_generic = vim.fn.expand(path .. '/' .. extension)
+
+    if vim.fn.filereadable(file_run) == 1 then
+        vim.cmd('!"' .. file_run .. '" "' .. name_full .. '"')
+    elseif vim.fn.filereadable(file_run_generic) == 1 then
+        vim.cmd('!"' .. file_run_generic .. '" "' .. name_full .. '"')
+    else
+        print(file .. ' and ' .. extension .. ' not found')
+    end
+end
+
+globals.run_project = function()
+    local file_run_generic = 'build.bat'
+
+    if vim.fn.filereadable(file_run_generic) == 1 then
+        vim.cmd('!"' .. file_run_generic .. '"')
+    else
+        print(file_run_generic .. ' not found')
+    end
+end
+
+local command_prefix = 'Global'
+vim.api.nvim_create_user_command(command_prefix .. 'GetFileNames', globals.get_file_names, {})
+return globals
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,43 +1,8 @@
-local get_file_names = function()
-    local keyword = vim.fn.input('Keyword: ')
+local status_ok, globals = pcall(require, 'core.globals')
 
-    vim.fn.jobstart('rg --files --sort path | rg --fixed-strings "' .. keyword .. '"', {
-        stdout_buffered = true,
-        on_stdout = function (_, data)
-            vim.api.nvim_buf_set_lines(0, 0, -1, false, data)
-        end ,
-    })
-end
-
-local export_current_file_name = function()
-    local name = vim.fn.expand('%:t')
-    vim.fn.setreg('+', name)
-    print('Copied file name to system clipboard')
-end
-
-local export_current_file_path = function()
-    local path = vim.fn.expand('%')
-    vim.fn.setreg('+', path)
-    print('Copied file path to system clipboard')
-end
-
-local run_current_file = function()
-    local extension = 'build.bat'
-    local delimiter = '_'
-    local path = vim.fn.expand('%:h')
-    local name_full = vim.fn.expand('%:t')
-    local name_cropped = vim.fn.expand('%:t:r')
-    local file = name_cropped .. delimiter .. extension
-    local file_run = vim.fn.expand(path .. '/' .. file)
-    local file_run_generic = vim.fn.expand(path .. '/' .. extension)
-
-    if vim.fn.filereadable(file_run) == 1 then
-        vim.cmd('!"' .. file_run .. '" "' .. name_full .. '"')
-    elseif vim.fn.filereadable(file_run_generic) == 1 then
-        vim.cmd('!"' .. file_run_generic .. '" "' .. name_full .. '"')
-    else
-        print(file .. ' and ' .. extension .. ' not found')
-    end
+if not status_ok then
+    print('globals not found')
+    -- return
 end
 
 -- General keymaps
@@ -61,13 +26,12 @@ vim.keymap.set('n', '<leader>_', '10<C-w>-', { desc = 'Expand window vertically'
 vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, { desc = 'Open diagnostics panel' })
 vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { desc = 'Open next diagnostics panel' })
 vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { desc = 'Open previous diagnostics panel' })
-vim.keymap.set('n', '<leader>rb', '<cmd>!build.bat<CR>', { desc = 'Run build script' })
+vim.keymap.set('n', '<leader>rb', globals.run_project, { desc = 'Run build script' })
 vim.keymap.set('n', '<leader>rB', ':!build.bat', { desc = 'Run build script' })
-vim.keymap.set('n', '<leader>rf', run_current_file, { desc = 'Run build script of current file' })
-vim.keymap.set('n', '<leader>xn', export_current_file_name, { desc = 'Export file name to system clipboard' })
-vim.keymap.set('n', '<leader>xp', export_current_file_path, { desc = 'Export file path to system clipboard' })
+vim.keymap.set('n', '<leader>rf', globals.run_current_file, { desc = 'Run build script of current file' })
+vim.keymap.set('n', '<leader>xn', globals.export_current_file_name, { desc = 'Export file name to system clipboard' })
+vim.keymap.set('n', '<leader>xp', globals.export_current_file_path, { desc = 'Export file path to system clipboard' })
 vim.keymap.set('v', '<leader>d', '"+d', { desc = 'Delete to system clipboard' })
 vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
 vim.keymap.set('v', '<leader>p', '"+p', { desc = 'Paste from system clipboard' })
 vim.keymap.set('v', '<leader>P', '"+P', { desc = 'Paste from system clipboard' })
-vim.keymap.set('n', '<leader>gf', get_file_names, {desc = 'Get files names with keyword'})
