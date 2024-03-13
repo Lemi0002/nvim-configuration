7cd78ac854760338cd02ae1209ea9accea89814e
Lemi0002 <51885072+Lemi0002@users.noreply.github.com>
2024-03-13 17:58:26 +0100

    Add keymaps

--- plugin/gitsigns_nvim.lua
+++ plugin/gitsigns_nvim.lua
@@ -8,3 +8,7 @@ end
 
 -- Configure settings
 gitsigns.setup()
+
+-- Package specific keymaps
+vim.keymap.set('n', '<leader>gn', gitsigns.next_hunk, { desc = 'Got to next git hunk' })
+vim.keymap.set('n', '<leader>gp', gitsigns.prev_hunk, { desc = 'Got to previous git hunk' })
