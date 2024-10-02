601f5b5a88d901d833be872d5b7c5a0aa429539f
Lemi0002 <51885072+Lemi0002@users.noreply.github.com>
2024-10-02 17:54:10 +0200

    Add keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -23,6 +23,7 @@ vim.keymap.set('n', '<leader>.', '10<C-w>>', { desc = 'Shrink window horizontall
 vim.keymap.set('n', '<leader>-', '10<C-w><', { desc = 'Expand window horizontally' })
 vim.keymap.set('n', '<leader>:', '10<C-w>+', { desc = 'Shrink window vertically' })
 vim.keymap.set('n', '<leader>_', '10<C-w>-', { desc = 'Expand window vertically' })
+vim.keymap.set('n', '<leader>m', '\'', { desc = 'Remap for marks' })
 vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, { desc = 'Open diagnostics panel' })
 vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { desc = 'Open next diagnostics panel' })
 vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { desc = 'Open previous diagnostics panel' })
@@ -36,3 +37,4 @@ vim.keymap.set('v', '<leader>p', '"+p', { desc = 'Paste from system clipboard' }
 vim.keymap.set('v', '<leader>P', '"+P', { desc = 'Paste from system clipboard' })
 vim.keymap.set('v', 'J', ':move \'>+1<CR>gv=gv', { desc = 'Move visually selected range down' })
 vim.keymap.set('v', 'K', ':move \'<-2<CR>gv=gv', { desc = 'Move visually selected range up' })
+vim.keymap.set('v', '<leader>m', '\'', { desc = 'Remap for marks' })
