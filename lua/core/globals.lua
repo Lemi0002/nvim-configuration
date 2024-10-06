149382c0b32974c5fc531b48db5fe4ec0a3bb097
Lemi0002 <51885072+Lemi0002@users.noreply.github.com>
2024-10-06 18:35:21 +0200

    Remove unused build functionality

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -28,7 +28,7 @@ globals.export_current_file_path = function()
     print('Copied file path to system clipboard')
 end
 
-globals.run_build = function(file_mode)
+globals.run_build = function()
     local configuration = {
         name = 'build',
         extensions = {
@@ -39,76 +39,24 @@ globals.run_build = function(file_mode)
         delimiter = '_',
     }
 
-    local file_path = vim.fn.expand('%:h')
-    local file_name = vim.fn.expand('%:t')
-    local file_name_root = vim.fn.expand('%:t:r')
     local file
     local file_extension
     local file_found = false
 
-    if file_mode == true then
-        for _, extension in ipairs(configuration.extensions) do
-            file = vim.fn.expand(
-                file_path ..
-                '/' ..
-                file_name_root ..
-                configuration.delimiter ..
-                configuration.name ..
-                extension.name
-            )
+    for _, extension in ipairs(configuration.extensions) do
+        file = vim.fn.expand(configuration.name .. extension.name)
 
-            if vim.fn.filereadable(file) == 1 then
-                file_found = true
-                file_extension = extension
-                goto file_found
-            end
+        if vim.fn.filereadable(file) == 1 then
+            file_found = true
+            file_extension = extension
+            break
         end
+    end
 
-        for _, extension in ipairs(configuration.extensions) do
-            file = vim.fn.expand(
-                file_path ..
-                '/' ..
-                configuration.name ..
-                extension.name
-            )
-
-            if vim.fn.filereadable(file) == 1 then
-                file_found = true
-                file_extension = extension
-                break
-            end
-        end
-
-        ::file_found::
-        if file_found then
-            vim.cmd('!' .. file_extension.command .. ' "' .. file .. '" "' .. file_name .. '"')
-        else
-            print(
-                '"' ..
-                file_name_root ..
-                configuration.delimiter ..
-                configuration.name ..
-                '" and local "' ..
-                configuration.name ..
-                '" not found'
-            )
-        end
+    if file_found then
+        vim.cmd('!' .. file_extension.command .. ' "' .. file .. '"')
     else
-        for _, extension in ipairs(configuration.extensions) do
-            file = vim.fn.expand(configuration.name .. extension.name)
-
-            if vim.fn.filereadable(file) == 1 then
-                file_found = true
-                file_extension = extension
-                break
-            end
-        end
-
-        if file_found then
-            vim.cmd('!' .. file_extension.command .. ' "' .. file .. '"')
-        else
-            print('"' .. configuration.name .. '" not found')
-        end
+        print('"' .. configuration.name .. '" not found')
     end
 end
 
